b6b3b31d3d026f600bb97dffc344533cb391eace
cks1031 <cks1031@naver.com>
2024-08-13 11:37:35 +0900

    pydantic - sungjuk 모델 자동 생성 및 상세 조회

--- pydantic01.py
+++ pydantic01.py
@@ -32,6 +32,38 @@ def index():
 def sj_readall():
     return sungjuk_db
 
+# 성적 데이터 추가
+# FastAPI swagger UI 이용 : http://127.0.0.1:8000/docs
+# 클라이언트가 요청시 성적데이터는 json으로 구성되어야 함
+@app.post('/sjadd', response_model=Sungjuk)
+def sj_create(sj: Sungjuk):
+    # sj = Sungjuk('혜교', 99,98,99).json()
+
+    sungjuk_db.append(sj)
+    return sj
+
+# 샘플 성적데이터 추가 : 3건의 기본 데이터
+@app.get('/sjadd', response_model=Sungjuk)
+def sj_create():
+    sj = Sungjuk(name='혜교', kor=99,eng=98,mat=99)
+    sungjuk_db.append(sj)
+
+    sj = Sungjuk(name='라라', kor=59,eng=68,mat=76)
+    sungjuk_db.append(sj)
+
+    sj = Sungjuk(name='누누', kor=29,eng=38,mat=49)
+    sungjuk_db.append(sj)
+    return sj
+
+# 성적데이터 상세 조회  - 이름으로 조회
+@app.get('/sjone/{name}',response_model=Sungjuk)
+def sjone(name: str):
+    findone = Sungjuk(name='none',kor=00,eng=00,mat=00)
+    for sj in sungjuk_db:
+        if sj.name == name:
+            findone = sj
+    return findone
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run('pydantic01:app', reload=True)
