3e887de00a53e7388fbfe1fee68bb7408ff063d2
cks1031 <cks1031@naver.com>
2024-08-14 12:04:35 +0900

    sqlalchemy - dbfactory를 비동기 켄텍스트 매니저로 실행

--- app/dbfactory.py
+++ app/dbfactory.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.settings import config
+from app.models import sungjuk, member
 
 engine = create_engine(config.sqlite_url, connect_args={}, echo=True)
 SessionLocal = sessionmaker(atocommit=False, autoflush=False, bind=engine)
 
-def db_startup():
+async def db_startup():
+    sungjuk.Base.metadata.create_all(engine)
+    member.Base.metadata.create_all(engine)
+
+async def db_shutdown():
     pass
